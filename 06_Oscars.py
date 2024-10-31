c7b50b4196a90df911500e57f6778bba39769598
Axelisin <33000201+Axelisin@users.noreply.github.com>
2024-10-31 16:11:28 +0200

    Update 06_Oscars.py

--- 06_Oscars.py
+++ 06_Oscars.py
@@ -11,7 +11,7 @@ for _ in range(judges):
     points_recieved = (len(judge_name) * judge_points) / 2
     total_points += points_recieved
     if total_points > NOMINEE:
-        print(f"Congratulations, {actor_name} got a nominee for leading role with {total_points:.2f}!")
+        print(f"Congratulations, {actor_name} got a nominee for leading role with {total_points:.1f}!")
         break
 
 # if total_points > NOMINEE:
@@ -20,4 +20,4 @@ for _ in range(judges):
 # this is for..else structure here, else used if no BREAK occured
 else:
     points_needed = NOMINEE - total_points
-    print(f"Sorry, {actor_name} you need {points_needed:.2f} more!")
+    print(f"Sorry, {actor_name} you need {points_needed:.1f} more!")
