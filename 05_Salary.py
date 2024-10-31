68842c6239e107e680f7d3f24338b93fc858bffe
Axelisin <33000201+Axelisin@users.noreply.github.com>
2024-10-31 15:20:10 +0200

    Create 05_Salary.py

---
+++ 05_Salary.py
@@ -0,0 +1,23 @@
+n = int(input())
+salary = int(input())
+
+for _ in range(n):
+    website = input()
+    if website == "Facebook":
+        salary -= 150
+        if salary <= 0:
+            break
+    elif website == "Instagram":
+        salary -= 100
+        if salary <= 0:
+            break
+    elif website == "Reddit":
+        salary -= 50
+        if salary <= 0:
+            break
+
+
+if salary <= 0:
+    print(f"You have lost your salary.")
+else:
+  print(int(salary))
