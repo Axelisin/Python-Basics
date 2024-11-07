2e58916628411c0cd0ca58c74cecc6f250be43f5
Axelisin <33000201+Axelisin@users.noreply.github.com>
2024-11-07 11:06:18 +0200

    Update 01. Old Books.py

--- While Loop - Exercise/01. Old Books.py
+++ While Loop - Exercise/01. Old Books.py
@@ -1,17 +1,19 @@
 target_book = input()
 
 checked_books = 0
+is_book_found = False
 
 current_book = input()
 while current_book != "No More Books":
     if current_book == target_book:
+        is_book_found = True
         break
 
     checked_books += 1    
     current_book = input()
 
-if current_book == "No More Books":
+if is_book_found:
+    print(f"You checked {checked_books} books and found it.")
+else:
     print("The book you search is not here!")
     print(f"You checked {checked_books} books.")
-else:
-    print(f"You checked {checked_books} books and found it.")
